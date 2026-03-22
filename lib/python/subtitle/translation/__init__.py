9ea82432fe63d9fa0aea48cef4b3c20027ba94d6
Amir SHIRALI POUR <sushiant60@gmail.com>
2026-03-22 20:09:48 +0100

    refactor(subtitle): extract translation resegmentation

--- lib/python/subtitle/translation/__init__.py
+++ lib/python/subtitle/translation/__init__.py
@@ -1,3 +1,4 @@
 from .parser import parse_translated_batch_output
+from .resegment import resegment_translation
 
-__all__ = ["parse_translated_batch_output"]
+__all__ = ["parse_translated_batch_output", "resegment_translation"]
