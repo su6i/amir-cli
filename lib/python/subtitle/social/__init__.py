b5287179aac55c5e811af0ae3f8ad496d3f61f7c
Amir SHIRALI POUR <emasalari@gmail.com>
2026-03-22 20:16:59 +0100

    refactor(subtitle): extract social metadata formatters

--- lib/python/subtitle/social/__init__.py
+++ lib/python/subtitle/social/__init__.py
@@ -1,7 +1,10 @@
 from .post_helpers import call_llm_for_post, sanitize_post, telegram_sections_complete
+from .metadata import compose_post_file_header, format_publish_date
 
 __all__ = [
     "call_llm_for_post",
+    "compose_post_file_header",
+    "format_publish_date",
     "sanitize_post",
     "telegram_sections_complete",
 ]
