62006e1da8466b8191327114d9437776f3c52729
Amir SHIRALI POUR <emasalari@gmail.com>
2026-03-22 20:08:01 +0100

    refactor(subtitle): extract segmentation helper utilities

---
+++ lib/python/subtitle/segmentation/__init__.py
@@ -0,0 +1,13 @@
+from .helpers import (
+    group_entries_into_paragraphs,
+    take_n_words_with_punct_snap,
+    take_words_up_to,
+    vis_len,
+)
+
+__all__ = [
+    "group_entries_into_paragraphs",
+    "take_words_up_to",
+    "take_n_words_with_punct_snap",
+    "vis_len",
+]
