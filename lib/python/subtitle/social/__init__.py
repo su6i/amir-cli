9c2a6e2c140b07edd9a64f8925e57eb7532f9df5
Amir SHIRALI POUR <emasalari@gmail.com>
2026-03-22 20:22:46 +0100

    refactor(subtitle): extract social prompt builder

--- lib/python/subtitle/social/__init__.py
+++ lib/python/subtitle/social/__init__.py
@@ -2,6 +2,7 @@ from .post_helpers import call_llm_for_post, sanitize_post, telegram_sections_co
 from .metadata import compose_post_file_header, format_publish_date
 from .generator import generate_posts
 from .discovery import discover_video_metadata
+from .prompts import get_post_prompt
 
 __all__ = [
     "call_llm_for_post",
@@ -9,6 +10,7 @@ __all__ = [
     "discover_video_metadata",
     "format_publish_date",
     "generate_posts",
+    "get_post_prompt",
     "sanitize_post",
     "telegram_sections_complete",
 ]
