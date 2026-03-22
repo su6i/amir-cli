17d01ad5d6d3889e9f5e518b542f763a1c2ea1f8
Amir SHIRALI POUR <sushiant60@gmail.com>
2026-03-22 20:29:12 +0100

    refactor(subtitle): extract final translation text-fix pass

--- lib/python/subtitle/translation/__init__.py
+++ lib/python/subtitle/translation/__init__.py
@@ -1,5 +1,6 @@
 from .parser import parse_translated_batch_output
 from .fallback_chain import translate_with_batch_fallback_chain
+from .postfix import apply_final_target_text_fixes
 from .resegment import resegment_translation
 from .single_attempt import translate_batch_single_attempt
 from .validation import validate_and_retry_translations
@@ -9,5 +10,6 @@ __all__ = [
 	"translate_batch_single_attempt",
 	"translate_with_batch_fallback_chain",
 	"validate_and_retry_translations",
+	"apply_final_target_text_fixes",
 	"resegment_translation",
 ]
